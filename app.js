a951ca1c110ee1709a7da47e5c3c3e23874c343d
Matheo Such <matheosuch1@gmail.com>
2024-07-22 20:52:44 -0300

    Mejora para deploy

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const dbConnection = require('./database/config');
 const cors = require('cors');
-const app = express();
 require('dotenv').config();
 
-app.use(express.json());
+const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.use('/auth', require('./router/authRouter'));
+dbConnection()
+	.then(() => {
+		app.use('/auth', require('./router/authRouter'));
+		app.use('/admin', require('./router/adminRouter'));
 
-app.use('/admin', require('./router/adminRouter'));
+		app.use((err, req, res, next) => {
+			console.error(err.stack);
+			res.status(500).send('Algo salió mal!');
+		});
 
-dbConnection();
-
-app.listen(process.env.PORT, () => {
-	console.info(`Escuchando puerto ${process.env.PORT}`);
-});
+		const PORT = process.env.PORT || 3000;
+		app.listen(PORT, () => {
+			console.info(`Escuchando puerto ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error('Error al conectar a la base de datos:', error);
+	});
