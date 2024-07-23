b7f3d0b71e672704cc12432e7440889f0047d558
Matheo Such <matheosuch1@gmail.com>
2024-07-22 22:09:56 -0300

    Correción de deploy

--- app.js
+++ app.js
@@ -6,29 +6,14 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-const corsOptions = {
-	origin: 'https://javapets.netlify.app',
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	credentials: true,
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
+app.use('/auth', require('./router/authRouter'));
 
-dbConnection()
-	.then(() => {
-		app.use('/auth', require('./router/authRouter'));
-		app.use('/admin', require('./router/adminRouter'));
+app.use('/admin', require('./router/adminRouter'));
 
-		app.use((err, req, res, next) => {
-			console.error(err.stack);
-			res.status(500).send('Algo salió mal!');
-		});
+dbConnection();
 
-		const PORT = process.env.PORT || 3000;
-		app.listen(PORT, () => {
-			console.info(`Escuchando puerto ${PORT}`);
-		});
-	})
-	.catch((error) => {
-		console.error('Error al conectar a la base de datos:', error);
-	});
+app.listen(process.env.PORT, () => {
+	console.info(`Escuchando puerto ${process.env.PORT}`);
+});
